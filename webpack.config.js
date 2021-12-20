6ffe51b986f403b366cded1c4ca44288d7a19213
Sumon Fakir <fakirsumon78@gmail.com>
2021-12-20 12:56:34 +0200

    adding lint and deploy with ci

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['./frontend/src/index.js'],
   output: {
     path: __dirname + '/build',
     filename: 'bundle.js',
